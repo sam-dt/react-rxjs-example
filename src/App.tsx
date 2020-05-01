3bd8f18418c08b3f81e4cf3f8f0108f572de6924
Sam Dhondt <sam.dhondt@codifly.be>
2020-05-01 13:11:05 +0200

    Implement fetch example

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,30 @@
 import React from 'react';
 import './App.css';
 import Search from './Search';
+import { useObservableState } from 'observable-hooks';
+import { fromFetch } from 'rxjs/fetch';
+import { map, switchMap, startWith, catchError, delay } from 'rxjs/operators';
+import { of } from 'rxjs';
 
-const data = [
-  'Lord of the Rings',
-  'The Ring',
-  'Ring of Fire',
-  'Star Wars',
-  'Star Trek',
-  'From the Stars',
-  'Black',
-  'Black Beauty',
-];
+interface User {
+  name: string;
+}
 
 const App = () => {
+  const search = useObservableState(
+    fromFetch('https://jsonplaceholder.typicode.com/users').pipe(
+      delay(1000),
+      switchMap(response => response.json()),
+      map((users: User[]) => users.map(user => user.name)),
+      map(users => (<Search data={users} />)),
+      startWith(<h2>Loading...</h2>),
+      catchError(() => of(<h2>Error!</h2>))
+    )
+  );
+
   return (
     <div className="App">
-      <Search data={data} />
+      {search}
     </div>
   );
 };
